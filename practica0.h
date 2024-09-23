3d2da671f935056b39d20a4dcc261fb07026dcc8
lageese <151532503+lageese@users.noreply.github.com>
2024-09-23 16:49:17 +0200

    .

---
+++ practica0.h
@@ -0,0 +1,8 @@
+//
+// Created by angela on 23/09/24.
+//
+
+#ifndef PRACTICA0_H
+#define PRACTICA0_H
+
+#endif //PRACTICA0_H
---
+++ staticlist.h
@@ -0,0 +1,41 @@
+//
+// Created by angela on 23/09/24.
+//
+
+#ifndef STATICLIST_H
+#define STATICLIST_H
+
+
+#include "types.h"
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define NULL (-1)
+#define MAX 25
+
+typedef int tPos;
+
+typedef tSong tItem;
+
+typedef struct {
+    tItem data[MAX];
+    tPosS lastPos;
+}tList;
+
+
+
+void createEmptyList(tList *L);
+void deleteAtPosition(tPos p, tList *L);
+bool isEmptyList(tList L);
+tPos first(tList L);
+tPos next(tPos p, tList L);
+tPos last(tList l);
+tPos previous(tPos p,tList L);
+tItem getItem(tPos p, tList L);
+void updateItem(tItem d, tPos p, tList *L);
+tPos findItem(tSongTitle d,tList L);
+
+
+
+#endif //STATICLIST_H
