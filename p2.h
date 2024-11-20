fe9fa4d3236349cc8a73c00549692f1cf1282a7d
Ángela García <151532503+lageese@users.noreply.github.com>
2024-11-20 20:19:31 +0100

    Update p2.h

--- p2.h
+++ p2.h
@@ -1,6 +1,4 @@
-//
-// Created by angela on 15/11/24.
-//
+
 
 #ifndef P2_H
 #define P2_H
@@ -17,35 +15,22 @@
 #include <time.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include "memlist.h"
 
 //CONSTANTES DEFINIDAS
 
 
 //FUNCIONES GENERALES P2
-void deallocateGen();
-void allocateGen();
-void memfill();
-void memdump();
+//void deallocateGen();
+//void allocateGen();
+//void memfill();
+//void memdump();
 void memoryGen();
-void readfile();
-void writefile();
-void writeC();
-void readC();
-void recurse(int n);
+//void readfile(char *trozos[]);
+void writefile(char *trozos[]);
+void writeC(char *trozos[]);
+//void readC(char *trozos[]);
+void recurse(char *trozos[]);
 
-
-//FUNCIONES "AUXILIARES" P2
-void Do_pmap (void);
-void Cmd_ReadFile (char *ar[]);
-ssize_t LeerFichero (char *f, void *p, size_t cont);
-void do_DeallocateDelkey (char *args[]);
-void do_AllocateMmap(char *arg[]);
-void * MapearFichero (char * fichero, int protection);
-void do_AllocateShared (char *tr[]);
-void do_AllocateCreateshared (char *tr[]);
-void * ObtenerMemoriaShmget (key_t clave, size_t tam);
-void LlenarMemoria (void *p, size_t cont, unsigned char byte);
-void ImprimirListaShared(tListM  * memList);
-void ImprimirListaMmap(tListM *memList);
 
 #endif //P2_H
