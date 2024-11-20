59c219dc38fd90eecd7bc7fa573d8a60f369e61a
Ángela García <151532503+lageese@users.noreply.github.com>
2024-11-20 20:15:03 +0100

    Update memlist.c

--- memlist.c
+++ memlist.c
@@ -4,84 +4,84 @@
 #include "memlist.h"
 
 void createEmptyMemList(tListM* memList) {
-  memList->lastPos=-1;
+    memList->lastPos=-1;
 
 }
 
 bool isEmptyMemList(tListM memList) {
-  return memList.lastPos==-1;
+    return memList.lastPos==-1;
 }
 
 int nextMemListPos(int pMem, tListM memList) {
-  if(pMem==memList.lastPos)
-    return -1;
-  else
-    return ++pMem;
+    if(pMem==memList.lastPos)
+        return -1;
+    else
+        return ++pMem;
 }
 
 int prevMemListPos(int pMem, tListM memList) {
-  return --pMem;
+    return --pMem;
 }
 
 int firstMemListPos( tListM memList) {
-  return 0;
+    return 0;
 }
 
 int lastMemListPos(tListM memList) {
-  return memList.lastPos;
+    return memList.lastPos;
 }
 
 
 bool insertMemListPos(tListM* memList, dataMem m) {
-  if(memList->lastPos<MAX_LISTMEM){
-    memList->lastPos++;
-    memList->itemM[memList->lastPos]=m;
-    return true;
-  }else
+    if(memList->lastPos<MAX_LISTMEM){
+        memList->lastPos++;
+        memList->itemM[memList->lastPos]=m;
+        return true;
+    }else
         return false;
 
 }
 
 
 dataMem getDataItemList(tListM memList, int pMem) {
-  return memList.itemM[pMem];
+    return memList.itemM[pMem];
 
 }
 
 
 void freeMemList(tListM *memList) {
-  int pos;
-  dataMem item;
+    int pos;
+    dataMem item;
 
-  for(pos=firstMemListPos(*memList);pos<=lastMemListPos(*memList);pos++){
-    item=getDataItemList(*memList,pos);
+    for(pos=firstMemListPos(*memList);pos<=lastMemListPos(*memList);pos++){
+        item=getDataItemList(*memList,pos);
 
-    if(item.cmdType==MALLOC)
-      free(item.dir);
-    else if(item.cmdType==SHARED){
-      if(shmget(item.Union.key,0,0)!=-1){
-        if(shmdt(item.dir)==-1)
-          return;
-        }
-    }else if(item.cmdType==MMAP)
-        munmap(item.dir, item.size);
-  }
+        if(item.cmdType==MALLOC)
+            free(item.dir);
+        else if(item.cmdType==SHARED){
+            if(shmget(item.Union.key,0,0)!=-1){
+                if(shmdt(item.dir)==-1)
+                    return;
+            }
+        }else if(item.cmdType==MMAP)
+            munmap(item.dir, item.size);
+    }
 
 }
 
-void deleteItemMemList( int pMem,tListM* memList) {
-  int i;
-  for(i=0; i<lastMemListPos(*memList);i++){
-    if(i>=pMem)
-      memList->itemM[i]=memList->itemM[i+1];
-  }
-  memList->lastPos--;
-}
-
-void deleteMemList(tListM* memList, int pMem) {
-  int i;
-  for(i=0;i<=memList->lastPos;i--){
-    deleteItemMemList(i,memList);
+void deleteItemMemList( int pMem,tListM *memList) {
+    int i;
+    for(i=0; i<lastMemListPos(*memList);i++){
+        if(i>=pMem)
+            memList->itemM[i]=memList->itemM[i+1];
+    }
     memList->lastPos--;
-  }
+}
+
+void deleteMemList(tListM* memList) {
+    int i;
+    for(i=0;i<=memList->lastPos;i--){
+        deleteItemMemList(i,memList);
+        memList->lastPos--;
+    }
 }
