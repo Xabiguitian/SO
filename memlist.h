62e28f0fe836ea6173cc426e9edbef34289151fc
Xabi <xabierguitianlopez@gmail.com>
2024-11-21 20:29:53 +0100

    Vuelva al commit de mediodía

--- memlist.h
+++ memlist.h
@@ -23,7 +23,7 @@ typedef struct fich {
 
 
 typedef enum cmd {
-    MALLOC,SHARED,MMAP
+    MALLOC, MMAP, SHARED
 } cmd;
 
 
@@ -31,7 +31,7 @@ typedef struct dataMem {
     size_t size;
     void * dir;
     char date[128];
-     int key;
+    int key;
     fich fichero;
     cmd cmdType;
 } dataMem;
