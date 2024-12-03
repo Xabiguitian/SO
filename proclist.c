82181a5e7c18e4c0f072d86b4b2b4e5334b447d8
Ángela García <151532503+lageese@users.noreply.github.com>
2024-12-03 18:06:28 +0100

    Create proclist.c

---
+++ proclist.c
@@ -0,0 +1,64 @@
+//
+// Created by angela on 3/12/24.
+//
+
+
+#include "proclist.h"
+
+void createEmptyProcList(tListProc *Proc) {
+    Proc->lastPos = -1;
+}
+
+bool isEmptyProcList(tListProc Proc) {
+    return Proc.lastPos == -1;
+}
+
+int firstProcList(tListProc Proc) {
+    return 0;
+}
+
+int lastProcList(tListProc Proc) {
+    return Proc.lastPos;
+}
+
+dataProc getItemProcList(int p, tListProc Proc) {
+    return Proc.itemP[p];
+}
+
+void deleteItemProcList(int p, tListProc *Proc) {
+    int i;
+
+    for(i = 0; i < lastProcList(*Proc); i++) {
+        if(i >= p)
+            Proc->itemP[i] = Proc->itemP[i+1];
+    }
+
+    Proc->lastPos--;
+}
+
+void deleteProcList(tListProc *Proc) {
+    int i;
+
+    for(i = 0; i <= Proc->lastPos; i++) {
+        deleteItemProcList(i, Proc);
+        Proc->lastPos--;
+    }
+}
+
+void updateItemProcList(dataProc item, int p, tListProc *Proc) {
+    int i;
+
+    for(i = 0; i <= lastProcList(*Proc); i++) {
+        if(i == p)
+            Proc->itemP[i] = item;
+    }
+}
+
+bool insertItemProcList(dataProc item, tListProc *Proc) {
+    if(Proc->lastPos < MAXDATA) {
+        Proc->lastPos++;
+        Proc->itemP[Proc->lastPos] = item;
+        return true;
+    } else
+        return false;
+}
