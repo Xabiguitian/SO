8f38a5d5113150bb61ff45a06f4de736773f874e
Ángela García <151532503+lageese@users.noreply.github.com>
2024-12-10 10:03:19 +0100

    Update searchlist.h

--- searchlist.h
+++ searchlist.h
@@ -6,31 +6,38 @@
 #define SEARCHDIRLIST_H
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <stdbool.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 
 #define MAXDIRS 100  // Número máximo de directorios en la lista
 #define MAXPATH 256  // Tamaño máximo de las rutas
+#define MAXNAME 4096
+
 
 // Estructura para la lista de búsqueda
 typedef struct {
     char dirs[MAXDIRS][MAXPATH]; // Almacena los directorios
     int lastPosSearch;                  // Número de directorios en la lista
 } tSearchList;
-
+tSearchList globalSearchList;
 void createEmptySearchList(tSearchList *searchList);
 bool isEmptySearchList(tSearchList searchList);
 int firstSearchList(tSearchList searchList);
 int lastSearchList(tSearchList searchList);
 int nextSearchList(tSearchList searchList,int pSearch);
-bool insertSearchList(tSearchList *searchList, char dir);
-void removeSearchList(tSearchList *searchList, char dir);
+bool insertSearchList(tSearchList *searchList, char *dir);
+void removeSearchList(tSearchList *searchList, char *dir);
 void removePositionSearchList(tSearchList *searchList, int position);
-tSearchList getItemSearchList(char dir);
-void updateSearchList(tSearchList *searchList, char dir);
+char *getItemSearchList(tSearchList searchList, int position);
+void updateSearchList(tSearchList *searchList, char *dir);
+char *Ejecutable(char *s);
 
 
 #endif //SEARCHDIRLIST_H
-
