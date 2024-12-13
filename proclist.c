0733bdfb38f1435eaae07ec5e3df638fc41d558b
Xabi <xabierguitianlopez@gmail.com>
2024-12-13 23:32:00 +0100

    back ya funciona

--- proclist.c
+++ proclist.c
@@ -17,28 +17,41 @@ int lastProcList(tListProc Proc) {
 }
 
 dataProc getItemProcList(int p, tListProc Proc) {
+    if (p < 0 || p > Proc.lastPos) {
+        fprintf(stderr, "Error: Índice fuera de rango en getItemProcList.\n");
+        exit(EXIT_FAILURE); // Manejo de error
+    }
     return Proc.itemP[p];
 }
 
-void deleteItemProcList(int p, tListProc *Proc) {
-    int i;
 
-    for(i = 0; i < lastProcList(*Proc); i++) {
-        if(i >= p)
-            Proc->itemP[i] = Proc->itemP[i+1];
+void deleteItemProcList(int p, tListProc *Proc) {
+    // Liberar memoria asociada a las propiedades dinámicas
+    if (Proc->itemP[p].user != NULL) {
+        free(Proc->itemP[p].user);
+    }
+    if (Proc->itemP[p].cmd != NULL) {
+        free(Proc->itemP[p].cmd);
+    }
+    if (Proc->itemP[p].date != NULL) {
+        free(Proc->itemP[p].date);
+    }
+
+    // Reorganizar la lista
+    for (int i = p; i < lastProcList(*Proc); i++) {
+        Proc->itemP[i] = Proc->itemP[i + 1];
     }
 
     Proc->lastPos--;
 }
 
-void deleteProcList(tListProc *Proc) {
-    int i;
 
-    for(i = 0; i <= Proc->lastPos; i++) {
-        deleteItemProcList(i, Proc);
-        Proc->lastPos--;
+void deleteProcList(tListProc *Proc) {
+    while (!isEmptyProcList(*Proc)) {
+        deleteItemProcList(firstProcList(*Proc), Proc);
     }
 }
+
 
 void updateItemProcList(dataProc item, int p, tListProc *Proc) {
     int i;
@@ -50,10 +63,11 @@ void updateItemProcList(dataProc item, int p, tListProc *Proc) {
 }
 
 bool insertItemProcList(dataProc item, tListProc *Proc) {
-    if(Proc->lastPos < MAXDATA) {
+    if (Proc->lastPos < MAXDATA - 1) { // Asegurar espacio disponible
         Proc->lastPos++;
         Proc->itemP[Proc->lastPos] = item;
         return true;
-    } else
+    } else {
         return false;
+    }
 }
